81d86ece6accca5a2d1c595430ad02d042bcc259
FuChai Mead <jessie.mf.wolff@gmail.com>
2017-12-05 09:00:30 -0700

    posting works

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,49 @@
+$(document).ready( function() {
+
+  $.ajax({
+    url: 'http://json-server.devpointlabs.com/api/v1/users',
+    method: 'GET',
+    dataType: 'JSON'
+  }).done( function(users) {
+    users.forEach( function(user) {
+      var list = $('.user-list')
+      var li = '<a><li data-user-id="' + user.id + '">' + user.first_name + '-' + user.last_name + ': ' + user.phone_number +'</li></a>'
+       list.append(li)
+    });
+  });
+
+  $('.a').on('click', function() {
+    $.ajax({
+      url: 'http://json-server.devpointlabs.com/api/v1/users',
+      type: 'POST',
+      dataType: 'JSON',
+    }).done( function() {
+      window.location.href = "/users/user.id/edit"
+      
+      console.log(success)
+    }).fail(function(data){
+      console.log
+    })
+  });
+    
+    $('#button').on('click', function() {
+      window.location.href = "/users/show"
+      var first_name = $('#first_name')
+      var last_name = $('#last_name')
+      var phone_number = $('#phone_number')
+      var data ={'user[first_name]': first_name[0].value, 'user[last_name]': last_name[0].value, 'user[phone_number]': phone_number[0].value}
+      $.ajax({
+        url: 'http://json-server.devpointlabs.com/api/v1/users',
+        type: 'POST',
+        dataType: 'JSON',
+        data: data
+      }).done( function(res) {
+        window.location.href = "/users"
+        console.log(res)
+      });
+
+    });
+  });
+
+
+
